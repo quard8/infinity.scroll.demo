c7c8a0f03cd429d76b53da00788980b42a7c106a
Irek Khasyanov <quard8@gmail.com>
2014-11-23 21:52:03 +0300

    rendering only visible +/- elements

--- js/infinityscroll.js
+++ js/infinityscroll.js
@@ -1,111 +1,160 @@
+/* global $, window */
+
 ;(function(factory) {
-	
-	var InfinityScroll = function(options) {
-		var $el = options.el,
-			$innerScroll = null,
-			sink = options.sink,
-			itemClass = options.itemClass,
-			template = options.template,
-			loading = false,
-			data = [],
-			proximityItemHeight = 0,
-			itemHeight = options.itemHeight,
-			visibleIndex = 0,
-			loadedIndex = 0,			
-			visibleCount = Math.ceil($el.height() / itemHeight) + 1;
 
-		var throttle = function(func, wait, options) {
-		    var context, args, result;
-		    var timeout = null;
-		    var previous = 0;
-		    options || (options = {});
-		    var later = function() {
-		      previous = new Date;
-		      timeout = null;
-		      result = func.apply(context, args);
-		    };
-		    return function() {
-		      var now = new Date;
-		      if (!previous && options.leading === false) previous = now;
-		      var remaining = wait - (now - previous);
-		      context = this;
-		      args = arguments;
-		      if (remaining <= 0) {
-		        clearTimeout(timeout);
-		        timeout = null;
-		        previous = now;
-		        result = func.apply(context, args);
-		      } else if (!timeout && options.trailing !== false) {
-		        timeout = setTimeout(later, remaining);
-		      }
-		      return result;
-		    };
-  		};
+    var ScrollItem = function(item, i, h) {
+
+        var height = h,
+            y = i * height,
+            data = item,
+            index = i;
+        return {
+            y: function() {
+                return y;
+            },
+            height: function() {
+                return height;
+            },
+            getData: function() {
+                return data;
+            },
+            index: function() {
+                return index;
+            }
+        }
+    };
+
+    var InfinityScroll = function(options) {
+        var $el = options.el,
+            $innerScroll = null,
+            sink = options.sink,
+            template = options.template,
+            loading = false,
+            itemHeight = options.itemHeight,
+            limit = options.limit || 30,
+            visibleIndex = 0,
+            data = [],
+            prefetchCount = 6,
+            visibleCount = Math.ceil($el.height() / itemHeight),
+            cb = options.positionUpdated;
 
 
-	    function scrollHandler() {
-	    	var checkPoint = 100,
-	    		scrollTop = $el.scrollTop();
-	    		visibleIndex = Math.ceil(scrollTop / itemHeight);
-	    		
-	    	if( !loading && scrollTop + $el.height() + checkPoint > $el[0].scrollHeight ) {
-	        	fetch(40);
-	        } else {
-	        	loadData();
-	        }
-	    }
+        var throttle = function(func, wait, options) {
+            var context, args, result;
+            var timeout = null;
+            var previous = 0;
+            options || (options = {});
+            var later = function() {
+              previous = new Date;
+              timeout = null;
+              result = func.apply(context, args);
+            };
+            return function() {
+              var now = new Date;
+              if (!previous && options.leading === false) previous = now;
+              var remaining = wait - (now - previous);
+              context = this;
+              args = arguments;
+              if (remaining <= 0) {
+                clearTimeout(timeout);
+                timeout = null;
+                previous = now;
+                result = func.apply(context, args);
+              } else if (!timeout && options.trailing !== false) {
+                timeout = setTimeout(later, remaining);
+              }
+              return result;
+            };
+        };
 
-	    function fetch(limit) {
-	    	loading = true;
-	    	sink.fetch(limit, function() {
-	    		$innerScroll.height(sink.count() * itemHeight);
-	    		loadData();
-	    		loading = false;
-	    	});
-	    }	
+        function scrollHandler() {
+            var checkPoint = 100,
+                scrollTop = $el.scrollTop();
+                visibleIndex = Math.ceil(scrollTop / itemHeight);
 
-	    function loadData() {
-	    	data = sink.get(visibleIndex, visibleCount);
-	    	render();
-	    }
+            if( !loading && scrollTop + $el.height() + checkPoint > $el[0].scrollHeight ) {
+                fetch()
+            } else {
+                render();
+            }
+        }
 
-	    function render() {
+        function fetch() {
+            loading = true;
+            sink.fetch(data.length, limit, function(items) {
+                for (var i = 0; i < items.length; i++) {
+                    data.push(new ScrollItem(items[i], data.length, itemHeight));
+                }
 
-	    	
+                $innerScroll.height(data.length * itemHeight);
+                render();
+                loading = false;
+            });
+        }
+
+        var current = [];
+
+        function render() {
+
+            var old = current;
+            current = [];
+
+            var v = visibleIndex - prefetchCount <= 0 ? 0 : visibleIndex - prefetchCount,
+                html = '', item;
+            for (var i = v; i < v + visibleCount + prefetchCount * 2; i++) {
+                if (i < data.length) {
+                    item = data[i];
+                    if ($innerScroll.find('div[data-idx="' + item.index() + '"]').length === 0) {
+                        var top = item.y();
+                        html += '<div class="row" data-idx="' + i + '" style="top: ' + top + 'px">' + template(item.getData()) + '</div>'
+                    }
+
+                    current.push(item);
+                }
+
+            }
+
+            $innerScroll.append(html);
+            var remove = [];
+            for (i = 0; i < old.length; i++) {
+                item = old[i];
+                if (current.indexOf(item) == -1) {
+                    remove.push($innerScroll.find('div[data-idx="' + item.index() + '"]'));
+                }
+            }
+
+            if (remove.length) {
+                var $stuff = $();
+                for (i = 0; i < remove.length; i++) {
+                    $stuff = $stuff.add(remove[i]);
+                }
+                $stuff.remove();
+            }
+
+
+            if (cb) {
+                //we always see 1 row less
+                cb(visibleIndex, visibleCount + visibleIndex - 1, data.length);
+            }
+        }
 
 
 
+        function init() {
+            $innerScroll = $('<div class="innerscroll"></div>');
+            $el.append($innerScroll);
+            $el.on('scroll', throttle(scrollHandler, 100));
+            //load data for first time
+            fetch();
+        }
 
-	    	$innerScroll.html('');
-	    	var offsetTop = $el[0].offsetTop;
-    		for (var i = 0; i < data.length; i++) {
-    			var top = (visibleIndex + i - 1) * itemHeight; 
-    			var item = $(template(data[i])).css({
-    				top: top + 'px'
-    			});
-    			$innerScroll.append(item);
-    			
-    		};	
-    				    		
+        init();
 
-	    }
-	    
-		function init() {
-			$innerScroll = $('<div class="innerscroll"></div>');
+        return {
+            position: function() {
 
-			$el.append($innerScroll);
-			$el.on('scroll', throttle(scrollHandler, 200));
-			//load data for first time
-			fetch(40);
-		}
-
-		init();
-
-		return {
-			position: function() {
-
-			}
-		};
-	};
-	factory.InfinityScroll = InfinityScroll;
+            }
+        };
+    };
+    factory.InfinityScroll = InfinityScroll;
 })(window);
--- js/sink.js
+++ js/sink.js
@@ -1,30 +1,20 @@
 ;(function(factory) {
-	factory.InfinityScrollTestSink = function() {
+    factory.InfinityScrollTestSink = function() {
 
-		var data = [];
+        return {
+            fetch: function(offset, limit, cb) {
+                var data = [];
+                for (var i=0; i<limit; i++) {
+                    var id = i + offset;
+                    data.push({
+                      id: id,
+                      name: "Name " + id,
+                      description: "Description " + id
+                    });
+                }
 
-
-		return {
-			get: function(offset, limit) {
-				
-				return data.slice(offset, offset + limit);
-			},
-			fetch: function(limit, cb) {				
-				for (var i=0; i<limit; i++) {
-				    var id = data.length;
-				    data.push({
-				      id: id,
-				      name: "Name " + id,
-				      description: "Description " + id
-				    });
-				}
-								
-				cb.apply();
-			},
-			count: function() {
-				console.log(data.length);
-				return data.length;
-			}
-		}
-	};
+                cb(data);
+            }
+        }
+    };
 })(window);
